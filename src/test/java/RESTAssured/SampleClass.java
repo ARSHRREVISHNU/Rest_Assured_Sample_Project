d6c29710bdac22b67ab3aea8062a7c9d3da3753e
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2023-05-01 09:57:48 +0530

    Initial Commit

---
+++ src/test/java/RESTAssured/SampleClass.java
@@ -0,0 +1,13 @@
+package RESTAssured;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleClass {
+
+    @Test
+    public void testMethod(){
+        Assert.assertEquals(1,1);
+    }
+
+}
